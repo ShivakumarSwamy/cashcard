f1d10f15dcdf72e4ad5c1d7cb1de66df8af0e3ad
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2024-01-27 21:25:52 +0530

    implementing GET

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,41 @@
 package example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
+	@Autowired
+	TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	void shouldBe200WhenCashCardIsFetched() {
+		ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/cashcards/99", String.class);
+
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(responseEntity.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isEqualTo(99);
+
+		Number amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
 	}
 
+	@Test
+	void shouldBe404WhenCashCardHasUnknownId() {
+		ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(responseEntity.getBody()).isBlank();
+	}
 }
